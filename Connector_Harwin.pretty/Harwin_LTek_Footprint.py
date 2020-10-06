2a2e3b5a4e42fc2bc5eb74fae4d77763ffe74cbe
Nicholas Limparis <nicholas@github.limpar.is>
2020-10-06 11:42:16 -0400

    Create Harwin_LTek_Footprint.py
    first part of footprint script for Harwinf M80 connectors. generates through hole dual and single row vertical connectors pads. no outlines, silkscreen or remainder of the footprint yet

---
+++ Connector_Harwin.pretty/Harwin_LTek_Footprint.py
@@ -0,0 +1,178 @@
+"""Harwin LTek Footprint generator
+
+
+
+This file can also be imported as a module and contains the following
+functions:
+
+    * fileExists - is used to error check for files that already exist
+     ask if it should be overwritten 
+    * main - Is used to generate the appropriate class members and 
+    then print them to a file
+"""
+
+import argparse
+
+import io
+"example using unix LF f=io.open('file.txt','w', newline='\n')"
+
+
+class HarwinLtekFootprint:
+    """
+    A class for making Harwin LTek footprints
+
+    '''
+    Attributes
+    -----------
+    pins : int
+        An integer number of pins (Default 2)
+    rows : int
+        Number of Rows (Default 1)
+    pitch : double
+        Pin pitch in mm (Default 2.00mm)
+    vert : bool
+        If true then the part is Vertical else Right Angle
+    strain : bool
+        If true add strain relief clips else don't
+    smd : bool
+        If true SMD pads will be returned instead of TH pad
+    thPinDim : List of doubles 
+        Pin Dimensions th
+    smdPadDim : List of doubles
+        list of doubles defining the SMD Pad dimensions
+    strainPinDim = list of doubles
+        list of doubles defining the pad dimensions for
+    partName : str
+        The part name string
+    ioFile : io
+        The file object where the file is to be saved
+    padString : String
+        A formated string to fill out the pin information
+
+    Methods
+    ----------
+    partName()
+        returns the formatted partname string
+    description()
+        returns the descr line of the model
+    strainRelief()
+        returns the strings for the strain relief if self.strain == true else returns ""
+    printPins()
+        returns the pad locations for the part
+    printFootPrint()
+        Returns the footprint file text for the whole footprint
+    printOutline()
+        returns the outline and silkscreen information
+    printModel()
+        Returns the model string line
+    setPadDim(padDim)
+        Sets the pad Dimensions for either SMD or TH
+    setStrainPad(padDim)
+        Sets Strain pad dimensions
+    """
+
+    partName = "Harwin_LTek-Male_{pins}"
+    thPinDim = [1.35, 1.35, 0.8]
+    strainPinDim = [1.5, 1.5, 0.95]
+    padString = "(pad {padID} {thSMD} {padShape} (at {xCoord} {yCoord}) (size {xSize} {ySize}) {drill} (layers {layers}.Cu {layers}.Mask))"
+
+
+
+    def __init__(self, pins=2, rows=1, pitch=2.00, vert = True, strain = True, smd = False ) -> None:
+        """
+        Parameters
+        ----------
+
+        pins : int
+            An integer number of pins (Default 2)
+        rows : int
+            Number of Rows (Default 1)
+        pitch : double
+            Pin pitch in mm (Default 2.00mm)
+        vert : bool
+            If true then the part is Vertical else Right Angle
+        strain : bool
+            If true add strain relief clips else don't
+        smd : bool
+            If true SMD pads will be returned instead of TH pad
+        myFile : str list
+            This is a list of strings that would make up the contents of the footprint file
+        """
+        self.pins = pins
+        self.rows = rows
+        self.pitch = pitch
+        self.vert = vert
+        self.strain = strain
+        self.smd = smd
+
+    def printPins(self):
+        """
+        Prints the pin/pad lines for the footprint with Pin 1 at 0,0 and pins folowing the pattern defined by Harwin positive of 0,0
+        returns a python list of strings
+
+        Parameters
+        ----------
+        None
+
+        Raises
+        ------
+        None
+
+        Returns
+        -------
+        myPins
+            A list of strings for all of the pads/pins
+        """
+
+        i = 0
+        myPins = []
+        myPadShape = "rect"
+        if self.smd:
+            myTH = "smd"
+            myLayers = "F.Cu F.Paste F.Mask"
+            #need to figure out the 
+        else:
+            myTH = "thru_hole"
+            myLayers = "*"
+            myPinsNum = self.pins
+            myPins.append(self.padString.format(padID= str(i), thSMD=myTH, padShape=myPadShape, xCoord= str(0.00), yCoord= str(0.00), xSize=str(self.thPinDim[0]), ySize=str(self.thPinDim[1]), drill= "(drill " + str(self.thPinDim[2]) + ") ", layers=myLayers))
+            myPadShape  = "circle"
+            if self.rows == 2:
+                myPinsNum = self.pins/2 #since there are 2 rows each row has half the pins.
+                myPins.append(self.padString.format(padID= str(myPinsNum+i), thSMD=myTH, padShape=myPadShape, xCoord= str(0.00), yCoord= str(self.pitch), xSize=str(self.thPinDim[0]), ySize=str(self.thPinDim[1]), drill= "(drill " + str(self.thPinDim[2]) + ") ", layers=myLayers))
+                
+
+            while i < myPinsNum:
+                myXCoord = (float(i) * self.pitch)
+                myPins.append(self.padString.format(padID= str(i), thSMD=myTH, padShape=myPadShape, xCoord= str(myXCoord), yCoord= str(0.00), xSize=str(self.thPinDim[0]), ySize=str(self.thPinDim[1]), drill= "(drill " + str(self.thPinDim[2]) + ") ", layers=myLayers))
+                if self.rows == 2:
+                    myPins.append(self.padString.format(padID= str(myPinsNum+i), thSMD=myTH, padShape=myPadShape, xCoord= str(0.00), yCoord= str(self.pitch), xSize=str(self.thPinDim[0]), ySize=str(self.thPinDim[1]), drill= "(drill " + str(self.thPinDim[2]) + ") ", layers=myLayers))
+                i += 1
+            
+            if self.strain:
+                #add the strain relief
+                myPins.append(self.padString.format(padID= "\"\"", thSMD=myTH, padShape=myPadShape, xCoord= str(myXCoord), yCoord= str(0.00), xSize=str(self.strainPinDim[0]), ySize=str(self.strainPinDim[1]), drill= "(drill " + str(self.strainPinDim[2]) + ") ", layers=myLayers))
+
+
+
+        return myPins
+
+
+
+
+
+def main():
+    parser = argparse.ArgumentParser(description=__doc__)
+    parser.add_argument(
+        'input_file',
+        type=str,
+        help="The spreadsheet file to pring the columns of"
+    )
+    args = parser.parse_args()
+    get_spreadsheet_cols(args.input_file, print_cols=True)
+
+
+if __name__ == "__main__":
+    main()
+    
+
